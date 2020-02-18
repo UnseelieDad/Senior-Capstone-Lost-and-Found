97c2ed214a0c822f0903e8aec4b2c6ea3deb2084
Nick Jones <Nick.Jones@pioneerrx.net>
2020-02-18 12:17:29 -0600

    Added initial PIN and admin pages.

--- LostAndFound/LostAndFound/Views/MenuPage.xaml.cs
+++ LostAndFound/LostAndFound/Views/MenuPage.xaml.cs
@@ -21,7 +21,8 @@ namespace LostAndFound.Views
             menuItems = new List<HomeMenuItem>
             {
                 new HomeMenuItem {Id = MenuItemType.Browse, Title="Home" },
-                new HomeMenuItem {Id = MenuItemType.About, Title="About" }
+                new HomeMenuItem {Id = MenuItemType.About, Title="About" },
+                new HomeMenuItem {Id = MenuItemType.Pin, Title="Admin" }
             };
 
             ListViewMenu.ItemsSource = menuItems;
---
+++ LostAndFound/LostAndFound/Views/PinPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using LostAndFound.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LostAndFound.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PinPage : ContentPage
+    {
+        private const string Pin = "8671";
+
+        MainPage RootPage { get => Application.Current.MainPage as MainPage; }
+
+        public PinPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Entry_Completed(object sender, EventArgs e)
+        {
+            if (sender is Entry entry)
+            {
+                if (entry.Text == Pin)
+                {
+                    //await RootPage.NavigateFromMenu((int)MenuItemType.Admin);
+                    await Navigation.PushAsync(new NavigationPage(new AdminPage()));
+                }
+            }
+        }
+    }
+}
