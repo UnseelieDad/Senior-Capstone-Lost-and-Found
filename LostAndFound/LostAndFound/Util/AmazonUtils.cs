2df355958dcaae79ab90ed402e8023ff33b1775d
UnseelieDad <48892273+UnseelieDad@users.noreply.github.com>
2020-02-19 02:38:12 -0600

    Adding congnito identity config

---
+++ LostAndFound/LostAndFound/Util/AmazonUtils.cs
@@ -0,0 +1,32 @@
+﻿using Amazon;
+using Amazon.CognitoIdentity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LostAndFound.Util
+{
+    class AmazonUtils
+    {
+        private static CognitoAWSCredentials _credentials;
+
+        public static CognitoAWSCredentials Credentials
+        {
+            get
+            {
+                if(_credentials == null)
+                {
+                    // Initialize the Amazon Cognito credentials provider
+                    _credentials = new CognitoAWSCredentials("us-east-2:79d460fe-0b66-4cc8-82b4-6fa77748f00c", // Identity pool ID 
+                        RegionEndpoint.USEast2 // Region
+                        );
+
+                }
+
+                return _credentials;
+            }
+
+        }
+            
+    }
+}
