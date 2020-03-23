48ce5ead80fd36a87f22eb848dec37ab872fa3ff
DValcho <daniel.valcho@pioneerrx.com>
2020-03-23 10:46:20 -0500

    Item Class Adjustments

--- LostAndFound/LostAndFound/Models/Item.cs
+++ LostAndFound/LostAndFound/Models/Item.cs
@@ -4,8 +4,17 @@ namespace LostAndFound.Models
 {
     public class Item
     {
-        public string Id { get; set; }
+        public string id { get; set; }
         public string Text { get; set; }
         public string Description { get; set; }
+        public string Email { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string Color { get; set; }
+        public string Type { get; set; }
+        public string Location { get; set; }
+        public DateTime DateLost { get; set; }
+        public string Status { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
     }
 }
