b9e86d106d3b1392817d1e0d04ddbfb28271936e
DValcho <daniel.valcho@pioneerrx.com>
2020-03-23 12:24:05 -0500

    Removed Sample Data, changed Id to id

--- LostAndFound/LostAndFound/Services/MockDataStore.cs
+++ LostAndFound/LostAndFound/Services/MockDataStore.cs
@@ -14,25 +14,24 @@ namespace LostAndFound.Services
         {
             items = new List<Item>()
             {
-                new Item { Id = Guid.NewGuid().ToString(), Text = "First item", Description="This is an item description." },
-                new Item { Id = Guid.NewGuid().ToString(), Text = "Second item", Description="This is an item description." },
-                new Item { Id = Guid.NewGuid().ToString(), Text = "Third item", Description="This is an item description." },
-                new Item { Id = Guid.NewGuid().ToString(), Text = "Fourth item", Description="This is an item description." },
-                new Item { Id = Guid.NewGuid().ToString(), Text = "Fifth item", Description="This is an item description." },
-                new Item { Id = Guid.NewGuid().ToString(), Text = "Sixth item", Description="This is an item description." }
+                //new Item { Id = Guid.NewGuid().ToString(), Text = "First item", Description="This is an item description." },
+                //new Item { Id = Guid.NewGuid().ToString(), Text = "Second item", Description="This is an item description." },
+                //new Item { Id = Guid.NewGuid().ToString(), Text = "Third item", Description="This is an item description." },
+                //new Item { Id = Guid.NewGuid().ToString(), Text = "Fourth item", Description="This is an item description." },
+                //new Item { Id = Guid.NewGuid().ToString(), Text = "Fifth item", Description="This is an item description." },
+                //new Item { Id = Guid.NewGuid().ToString(), Text = "Sixth item", Description="This is an item description." }
             };
         }
 
         public async Task<bool> AddItemAsync(Item item)
         {
             items.Add(item);
-
             return await Task.FromResult(true);
         }
 
         public async Task<bool> UpdateItemAsync(Item item)
         {
-            var oldItem = items.Where((Item arg) => arg.Id == item.Id).FirstOrDefault();
+            var oldItem = items.Where((Item arg) => arg.id == item.id).FirstOrDefault();
             items.Remove(oldItem);
             items.Add(item);
 
@@ -41,7 +40,7 @@ namespace LostAndFound.Services
 
         public async Task<bool> DeleteItemAsync(string id)
         {
-            var oldItem = items.Where((Item arg) => arg.Id == id).FirstOrDefault();
+            var oldItem = items.Where((Item arg) => arg.id == id).FirstOrDefault();
             items.Remove(oldItem);
 
             return await Task.FromResult(true);
@@ -49,7 +48,7 @@ namespace LostAndFound.Services
 
         public async Task<Item> GetItemAsync(string id)
         {
-            return await Task.FromResult(items.FirstOrDefault(s => s.Id == id));
+            return await Task.FromResult(items.FirstOrDefault(s => s.id == id));
         }
 
         public async Task<IEnumerable<Item>> GetItemsAsync(bool forceRefresh = false)
