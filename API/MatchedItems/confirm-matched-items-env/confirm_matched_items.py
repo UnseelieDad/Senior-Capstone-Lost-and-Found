751b999fa040134d1740dbc255677bfc2846c74d
UnseelieDad <voiceofsethmartin@gmail.com>
2020-04-30 23:03:55 -0500

    Removing matched entries with already confirmed items

--- API/MatchedItems/confirm-matched-items-env/confirm_matched_items.py
+++ API/MatchedItems/confirm-matched-items-env/confirm_matched_items.py
@@ -35,12 +35,41 @@ logger.info("SUCCESS: Connection to RDS mysql instance succeeded.")
 
 def handler(event, context):
     data = {
-        "update_id": event['id']
+        "update_id": event['match_id']
     }
 
+    update_id = data["update_id"]
+
     with connection.cursor() as cursor:
-        cursor.execute(f"update MatchedItems set Confirmed = 1 where id = {data['update_id']}")
-        connection.commit()
+        
+        # pull lost and found column values from matched items
+        cursor.execute(f"SELECT mi.LostItem, mi.FoundItem FROM MatchedItems mi WHERE mi.id = {update_id}")
+        ids = cursor.fetchone()
+        lost_id = ids[0]
+        found_id = ids[1]
+
+        # Remove all other matches with the confirmed items
+        cursor.execute(f"DELETE FROM MatchedItems mi WHERE (mi.id != {update_id} AND mi.LostItem = {lost_id}) OR (mi.id != {update_id} AND mi.FoundItem = {found_id})")
+
+        # remove found item from found and item tables
+        cursor.execute(f"SELECT f.Item FROM Found f WHERE f.id = {found_id}")
+        found_item_id = cursor.fetchone()[0]
+        cursor.execute(f"DELETE FROM Found f WHERE f.id = {found_id}")
+        cursor.execute(f"DELETE FROM Item i WHERE i.id = {found_item_id}")
+        
+        # pull item ID from lost table
+        cursor.execute(f"SELECT l.Item FROM Lost l WHERE l.id = {lost_id}")
+        lost_item_id = cursor.fetchone()[0]
+
+        # update confirmed and lost item reference in matched items
+        cursor.execute(f"UPDATE MatchedItems SET Confirmed = 1, ItemId = {lost_item_id} WHERE id = {update_id}")
+
+        # remove lost table entry
+        cursor.execute(f"DELETE FROM Lost l WHERE l.id = {lost_id}")
+
+        
+        
+    connection.commit()
 
     return {
         'statusCode': 200,
