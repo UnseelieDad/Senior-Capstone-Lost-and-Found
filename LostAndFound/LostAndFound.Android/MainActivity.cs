308b2359840ca0562ffd9894038404fb6cdea5a4
RoboEmile <roboemile@gmail.com>
2020-04-30 20:14:57 -0500

    added icons for ios and android on a new branch

--- LostAndFound/LostAndFound.Android/MainActivity.cs
+++ LostAndFound/LostAndFound.Android/MainActivity.cs
@@ -11,7 +11,7 @@ using Amazon.Util;
 
 namespace LostAndFound.Droid
 {
-    [Activity(Label = "LostAndFound", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "LostAndFound", Icon = "@mipmap/my_icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
--- LostAndFound/LostAndFound.Android/Resources/Resource.designer.cs
+++ LostAndFound/LostAndFound.Android/Resources/Resource.designer.cs
@@ -6771,6 +6771,9 @@ namespace LostAndFound.Droid
 			// aapt resource value: 0x7F0C0002
 			public const int launcher_foreground = 2131492866;
 			
+			// aapt resource value: 0x7F0C0003
+			public const int my_icon = 2131492867;
+			
 			static Mipmap()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
